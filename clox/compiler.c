8ca334c610a9df4467c8ae24be8bc3e77c1fa196
Matthew Jeorrett <Matthew@wallet.services>
2020-05-11 20:10:39 +0100

    Add strings

--- clox/compiler.c
+++ clox/compiler.c
@@ -9,6 +9,7 @@
 #endif // DEBUG_PRINT_CODE
 
 #include "scanner.h"
+#include "object.h"
 
 typedef struct {
 	Token current;
@@ -174,6 +175,10 @@ static void number() {
 	emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void unary() {
 	TokenType operatorType = parser.previous.type;
 
@@ -207,7 +212,7 @@ ParseRule rules[] = {
   { NULL,     binary,    PREC_COMPARISON },       // TOKEN_LESS            
   { NULL,     binary,    PREC_COMPARISON },       // TOKEN_LESS_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING          
+  { string,     NULL,    PREC_NONE },       // TOKEN_STRING          
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
