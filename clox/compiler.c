d3aacc9adb6a625ba8a97fd84376ba3df76213e4
Matthew Jeorrett <Matthew@wallet.services>
2020-05-12 13:13:21 +0100

    add ability to define variables

--- clox/compiler.c
+++ clox/compiler.c
@@ -147,6 +147,10 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token* name) {
+	return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
 static void binary() {
 	TokenType operatorType = parser.previous.type;
 
@@ -191,6 +195,15 @@ static void string() {
 	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static void namedVariable(Token name) {
+	uint8_t arg = identifierConstant(&name);
+	emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable() {
+	namedVariable(parser.previous);
+}
+
 static void unary() {
 	TokenType operatorType = parser.previous.type;
 
@@ -223,7 +236,7 @@ ParseRule rules[] = {
   { NULL,     binary,    PREC_COMPARISON },       // TOKEN_GREATER_EQUAL   
   { NULL,     binary,    PREC_COMPARISON },       // TOKEN_LESS            
   { NULL,     binary,    PREC_COMPARISON },       // TOKEN_LESS_EQUAL      
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
+  { variable,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
   { string,     NULL,    PREC_NONE },       // TOKEN_STRING          
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND             
@@ -261,10 +274,6 @@ static void parsePrecedence(Precedence precedence) {
 		ParseFn infixRule = getRule(parser.previous.type)->infix;
 		infixRule();
 	}
-}
-
-static uint8_t identifierConstant(Token* name) {
-	return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
 }
 
 static uint8_t parseVariable(const char* errorMessage) {
