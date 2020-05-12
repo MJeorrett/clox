b57437476cacbd50d48c915aa034ceef224f287d
Matthew Jeorrett <Matthew@wallet.services>
2020-05-12 16:12:46 +0100

    dissalow variable shadowing in initialisation

--- clox/compiler.c
+++ clox/compiler.c
@@ -195,6 +195,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
 	for (int i = compiler->localCount - 1; i >= 0; i--) {
 		Local* local = &compiler->locals[i];
 		if (identifiersEqual(name, &local->name)) {
+			if (local->depth == -1) {
+				error("Cannot read local variable in its own initializer.");
+			}
 			return i;
 		}
 	}
@@ -210,7 +213,7 @@ static void addLocal(Token name) {
 
 	Local* local = &current->locals[current->localCount++];
 	local->name = name;
-	local->depth = current->scopeDepth;
+	local->depth = -1;
 }
 
 static void declareVariable() {
@@ -388,8 +391,13 @@ static uint8_t parseVariable(const char* errorMessage) {
 	return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+	current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
 	if (current->scopeDepth > 0) {
+		markInitialized();
 		return;
 	}
 
